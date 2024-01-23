bb94a05dc8324e8ad3d44332b090c77cdb1cd40f
cmindrelaxing <shakil.devcraft@gmail.com>
2024-01-23 19:03:38 +0600

    cart product added

--- redux-code-start/tutorial7.js
+++ redux-code-start/tutorial7.js
@@ -5,14 +5,23 @@ const GET_PRODUCTS = 'GET_PRODUCTS';
 const ADD_PRODUCT = 'ADD_PRODUCT';
 
 // cart constants
+const GET_CART_ITEMS = 'GET_CART_ITEMS';
+const ADD_CART_ITEM = 'ADD_CART_ITEM';
 
-
+// initial products state
 const initialProductState = {
     products: ['sugar', 'salt'],
     numberofProducts: 2,
 }
 
-// actions
+// initial cart state
+const initialCartState = {
+  cart: ["sugar"],
+  numberofProducts: 1,
+};
+
+
+// product actions
 const getProducts = () => {
     return {
         type: GET_PRODUCTS,
@@ -25,6 +34,21 @@ const addProduct = (product) => {
         payload: product
     }
 };
+
+// cart actions
+const getCart = () => {
+    return {
+        type: GET_CART_ITEMS,
+    }
+};
+
+const addCart = (product) => {
+    return {
+        type: ADD_CART_ITEM,
+        payload: product
+    }
+};
+
 
 // productReducer
 const productReducer = (state = initialProductState, action) => {
@@ -46,8 +70,30 @@ const productReducer = (state = initialProductState, action) => {
     }
 };
 
+// cartReducer
+const cartReducer = (state = initialCartState, action) => {
+    switch (action.type) {
+        case GET_CART_ITEMS:
+            
+            return {
+                ...state,
+            };
+        case ADD_CART_ITEM:
+            
+            return {
+              cart: [...state.cart, action.payload],
+              numberofProducts: state.numberofProducts + 1,
+            };
+    
+        default:
+            return state;
+    }
+};
+
+
+
 // store
-const store = createStore(productReducer);
+const store = createStore(cartReducer);
 
 store.subscribe(() => {
     console.log(store.getState());
@@ -60,3 +106,6 @@ store.subscribe(() => {
 // store.dispatch(addProduct('potato'));
 
 // cartReducer
+store.dispatch(getCart());
+store.dispatch(addCart('oil'));
+store.dispatch(addCart('water'));
