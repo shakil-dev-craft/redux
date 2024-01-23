3867f3f8a3dff1795e3921f3ada34c4ea8b6bc26
cmindrelaxing <shakil.devcraft@gmail.com>
2024-01-23 16:14:16 +0600

    start tutorial 6 class minites 2:04

---
+++ redux-code-start/tutorial6.js
@@ -0,0 +1,86 @@
+// working need
+// .........................
+
+const { createStore } = require("redux");
+
+// state - count = 0;
+// action - increment, decrement, reset
+// reducer
+// store
+
+// constants
+const INCREMENT = "INCREMENT";
+const DECREMENT = "DECREMENT";
+const RESET = "RESET";
+const INCREMENT_BY_VALUE = "INCREMENT_BY_VALUE";
+
+// initialize state
+const initialState = {
+  count: 0,
+};
+
+// actions
+const incrementCounterAction = () => {
+  return {
+    type: INCREMENT,
+  };
+};
+
+const decrementCounterAction = () => {
+  return {
+    type: DECREMENT,
+  };
+};
+
+const resetCounterAction = () => {
+  return {
+    type: RESET,
+  };
+};
+
+const incrementCounterByValue = (value) => {
+  return {
+    type: INCREMENT_BY_VALUE,
+    payload: value
+  };
+};
+
+// reducer
+const counterReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return {
+        ...state,
+        count: state.count + 1,
+      };
+    case DECREMENT:
+      return {
+        ...state,
+        count: state.count - 1,
+      };
+    case RESET:
+      return {
+        count: 0,
+      };
+    case RESET:
+      return {
+        count: 0,
+      };
+
+    default:
+      return state;
+  }
+};
+
+// store
+const store = createStore(counterReducer);
+
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+store.dispatch(incrementCounterAction());
+store.dispatch(incrementCounterAction());
+store.dispatch(decrementCounterAction());
+store.dispatch(resetCounterAction());
+store.dispatch(incrementCounterAction());
